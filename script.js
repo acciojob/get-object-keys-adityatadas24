4aeec8184a8369a1ce4482d574c6f76dbadf06e6
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-11-21 11:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //your JS code here. If required.
 let student ={
-	name:'aditya',
+	name: 'aditya',
 }
-console.log(student.getKeys());
+Object.prototype.getKeys = function() {
+  const keys = [];
+  for (const key in this) {
+    keys.push(key);
+  }
+  return keys;
+};
+
+const studentKeys = student.getKeys();
+
+
+console.log(studentKeys);
